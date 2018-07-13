19f2ff2721d939a78104e85bd9dbb4ae67938f00
Diego <dcharrezt@unsa.edu.pe>
2018-07-12 23:39:33 -0500

    cuda and cimg

--- cuImages/rgbToGrayScale.cu
+++ cuImages/rgbToGrayScale.cu
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <CImg.h>
+#include <iostream>
+#include <string>
+
+using namespace cimg_library;
+using namespace std;
 
 __global__
 void rgba_to_greyscale(const uchar4* const rgbaImage,
@@ -30,3 +36,43 @@ void your_rgba_to_greyscale(const uchar4* const h_rgbaImage,
 
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 }
+
+int main() {
+	std::string input_file = "images/lena.jpg"	
+	std::string output_file = "images/grey_lena.jpg"
+
+	uchar4				*h_rgbImage, *d_rgbImage;
+	unsigned char *h_greyImage, *d_greyImage;
+
+	CImg<unsigned char> image(input_file),
+		grayWeight(image.width(), image.height(), 1, 1, 0);
+	image_rows = image.width();
+	image_cols = image.height();
+
+
+// for all pixels x,y in image
+  cimg_forXY(image,x,y) {
+    imgR(x,y,0,0) = image(x,y,0,0),    // Red component of image sent to imgR
+    imgG(x,y,0,1) = image(x,y,0,1),    // Green component of image sent to imgG
+    imgB(x,y,0,2) = image(x,y,0,2);    // Blue component of image sent to imgB:
+
+    // Separation of channels
+    int R = (int)image(x,y,0,0);
+    int G = (int)image(x,y,0,1);
+    int B = (int)image(x,y,0,2);
+    // Arithmetic addition of channels for gray
+    int grayValue = (int)(0.33*R + 0.33*G + 0.33*B);
+    // Real weighted addition of channels for gray
+    int grayValueWeight = (int)(0.299*R + 0.587*G + 0.114*B);
+    // saving píxel values into image information
+    gray(x,y,0,0) = grayValue;
+    grayWeight(x,y,0,0) = grayValueWeight;
+}
+  
+  grayWeight.save( "greys.jpg" );
+
+	
+  
+
+}
+
